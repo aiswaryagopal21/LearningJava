676a857a7ffd72e41352e5829a9190b8ddbf7602
“aiswarya.gopal” <aiswarya.gopal@bounteous.com>
2025-03-06 09:24:00 +0530

    2nd program

--- src/ex_01_javaBasics/Lab001_HelloWorldprogram.java
+++ src/ex_01_javaBasics/Lab001_HelloWorldprogram.java
@@ -2,6 +2,6 @@ package ex_01_javaBasics;
 
 public class Lab001_HelloWorldprogram {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello world");
     }
 }
